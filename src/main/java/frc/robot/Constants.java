7fff68f6a538471587ff4e926377dc276665763b
7064 <gametimebaby6@gmail.com>
2026-03-04 15:25:19 -0600

    New constants :)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -50,17 +50,20 @@ public final class Constants
     public static final double RIGHT_X_DEADBAND = 0.1;
     public static final double TURN_CONSTANT    = 6;
   }
+  public static class ClimberConstants {
+    public static final int kClimberMotorId = 36; // temporary value, will need to be updated when climber is implemented
+  }
    
   public static class ShooterConstants {
     // 2 Neos, 4in shooter wheels
-    public static final int kLeaderMotorId = 15999;
-    public static final int kFollowerMotorId = 16999; //  temporary values, will need to be updated when shooter is implemented
+    public static final int kLeaderMotorId = 31;
+    public static final int kFollowerMotorId = 32; //  temporary values, will need to be updated when shooter is implemented
   }
   
   public static class TurretConstants {
     // 1 Neo, 6.875 in diameter, 4:1 gearbox, 10:1 pivot gearing, non-continuous
     // 360 deg
-    public static final int kMotorId = 179999; // temporary value, will need to be updated when turret is implemented
+    public static final int kMotorId = 35; // temporary value, will need to be updated when turret is implemented
   }
    public static class HopperConstants {
     public static final int kHopperMotorId = 10000; // temporary value, will need to be updated when hopper is implemented
